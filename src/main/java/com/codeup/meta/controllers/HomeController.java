005ceeee346d35a3a7b0e4904ccc7d7acbee84d1
JoePerez11 <perez.joseph.k@gmail.com>
2022-07-16 17:23:42 -0500

    first commit

--- src/main/java/com/codeup/meta/controllers/HomeController.java
+++ src/main/java/com/codeup/meta/controllers/HomeController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 
-//Joe's Code
+//Joe's Code hehe
 
 @Controller
 public class HomeController {
