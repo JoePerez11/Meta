9c28aae009d4644ca6ecfda22db12283f83775cb
JoePerez11 <perez.joseph.k@gmail.com>
2022-07-16 17:17:42 -0500

    testing my code

--- src/main/java/com/codeup/meta/controllers/HomeController.java
+++ src/main/java/com/codeup/meta/controllers/HomeController.java
@@ -1,14 +1,29 @@
-package com.codeup.meta.controllers;
+package com.codeup.meta.controllers;//package com.codeup.meta.controllers;
+//
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.ResponseBody;
+//
+//@Controller
+//public class HomeController {
+//    @GetMapping("/")
+//    @ResponseBody
+//    public String main() {
+//        return "This is the landing page!";
+//    }
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+
+//Joe's Code
+
 @Controller
 public class HomeController {
     @GetMapping("/")
     @ResponseBody
-    public String main() {
+    public String main(){
         return "This is the landing page!";
     }
 }
